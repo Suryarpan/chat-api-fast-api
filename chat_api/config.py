c1ffd9bc0d898a281329b9549ce4b845e9ddfefe
Suryarpan <52556381+Suryarpan@users.noreply.github.com>
2025-04-24 02:05:57 +0530

    deafult host and port

--- chat_api/config.py
+++ chat_api/config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     app_name: str = "Chat API"
     debug: bool = False
-    app_host: str
-    app_port: int
+    app_host: str = "127.0.0.1"
+    app_port: int = 5000
     # DB creds
     mysql_host: str = "127.0.0.1"
     mysql_port: int = 3306
