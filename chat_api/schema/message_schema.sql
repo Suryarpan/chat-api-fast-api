dfff1e85bdd3f56744f2e7f0fe4533c2f6788864
Suryarpan <52556381+Suryarpan@users.noreply.github.com>
2025-04-26 01:34:55 +0530

    remove user table

--- chat_api/schema/message_schema.sql
+++ chat_api/schema/message_schema.sql
@@ -3,18 +3,13 @@ DROP TABLE IF EXISTS message_type_meta;
 DROP TABLE IF EXISTS message_text;
 
 CREATE TABLE message_meta (
-    mssg_id SERIAL PRIMARY KEY,
-    from_pvt_id BIGINT UNSIGNED NOT NULL,
-    to_pvt_id BIGINT UNSIGNED NOT NULL,
+    mssg_id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    from_pvt_id BINARY(16) NOT NULL,
+    to_pvt_id BINARY(16) NOT NULL,
     mssg_status ENUM ('sent', 'delivered', 'read') NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    CONSTRAINT from_user_fk FOREIGN KEY (from_pvt_id) REFERENCES users(pvt_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    CONSTRAINT to_user_fk FOREIGN KEY (to_pvt_id) REFERENCES users(pvt_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE message_type_meta (
